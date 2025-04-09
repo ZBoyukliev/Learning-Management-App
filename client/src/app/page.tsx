ded5913e54649d7aa3f657426e04d8464b9e455b
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-04-09 23:29:56 +0300

    import non dashboard page in page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,10 @@
+import NonDashboardNavbar from "@/components/NonDashboardNavbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-red-500">
-      Hi
+    <div className="nondashboard-layout">
+       <NonDashboardNavbar />
     </div>
   );
 }
