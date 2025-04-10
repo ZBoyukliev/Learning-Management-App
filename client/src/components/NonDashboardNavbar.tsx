8691dad83b14ff251e6e6e27a44eae4dd44e89c4
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-04-10 20:03:43 +0300

    common changes

--- client/src/components/NonDashboardNavbar.tsx
+++ client/src/components/NonDashboardNavbar.tsx
@@ -4,20 +4,20 @@ import { Bell, BookOpen } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-const NonDashboardNavbar = () => {
+const NonDashboardNavbar: any = () => {
 
   return (
-    <nav className="nondashboard-navbar">
+    <nav className="nondashboard-navbar bg-customgreys-primarybg">
       <div className="nondashboard-navbar__container">
         <div className="nondashboard-navbar__search">
-          <Link href="/" className="nondashboard-navbar__brand" scroll={false}>
-            EDROH
+          <Link href="/" className="nondashboard-navbar__brand hover:text-customgreys-dirtyGrey;" scroll={false}>
+            ZARKOB
           </Link>
           <div className="flex items-center gap-4">
             <div className="relative group">
               <Link
                 href="/search"
-                className="nondashboard-navbar__search-input"
+                className="nondashboard-navbar__search-input bg-customgreys-secondarybg"
                 scroll={false}
               >
                 <span className="hidden sm:inline">Search Courses</span>
