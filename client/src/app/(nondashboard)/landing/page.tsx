94343ec0b4a39e595bf6719ebb6e048e7e5a1f84
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-04-18 15:40:15 +0300

    css changes

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,13 +1,12 @@
-"use client"
+"use client";
 
-import React from 'react';
+import React from "react";
 import { motion } from "framer-motion";
-import Link from 'next/link';
-import { useCarousel } from '@/hooks/useCarousel';
-import Image from 'next/image';
+import Link from "next/link";
+import { useCarousel } from "@/hooks/useCarousel";
+import Image from "next/image";
 
 const LandingComponent = () => {
-
   const currentImage = useCarousel({ totalImages: 3 });
   return (
     <motion.div
@@ -16,7 +15,7 @@ const LandingComponent = () => {
       transition={{ duration: 0.5 }}
       className="landing"
     >
-        <motion.div
+      <motion.div
         initial={{ y: 20, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         transition={{ duration: 0.5 }}
@@ -44,15 +43,42 @@ const LandingComponent = () => {
               fill
               priority={index === currentImage}
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-              className={`landing__hero-image ${
-                index === currentImage ? "landing__hero-image--active" : ""
-              }`}
+              className={`landing__hero-image ${index === currentImage ? "landing__hero-image--active" : ""
+                }`}
             />
           ))}
         </div>
       </motion.div>
-    </motion.div>
-  )
-}
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ amount: 0.3, once: true }}
+        className="landing__featured"
+      >
+        <h2 className="landing__featured-title">Featured Courses</h2>
+        <p className="landing__featured-description">
+          From beginner to advanced, in all industries, we have the right
+          courses just for you and preparing your entire journey for learning
+          and making the most.
+        </p>
 
-export default LandingComponent
+        <div className="landing__tags">
+          {[
+            "web development",
+            "enterprise IT",
+            "react nextjs",
+            "javascript",
+            "backend development",
+          ].map((tag, index) => (
+            <span key={index} className="landing__tag">
+              {tag}
+            </span>
+          ))}
+        </div>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default LandingComponent;
