705ec2dcdf0c15a9bfc0ccaabd531b3e265cab63
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-04-22 22:20:04 +0300

    create a footer component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,7 @@
 import NonDashboardNavbar from "@/components/NonDashboardNavbar";
 import LandingComponent from "./(nondashboard)/landing/page";
 import Image from "next/image";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
        <main className="nondashboard-layout__main">
         <LandingComponent />
       </main>
+      <Footer />
     </div>
   );
 }
