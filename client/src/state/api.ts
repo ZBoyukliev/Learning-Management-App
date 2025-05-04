63984e2c54a27530444d74e49cdf15ca2cf34133
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-05-04 15:28:26 +0300

    create a types folder and get course and course endpoints

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -4,7 +4,23 @@ export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL}),
     reducerPath: "api",
     tagTypes: ["Courses", "Users", "UserCourseProgress"],
-    endpoints: (build) => ({}),
+    endpoints: (build) => ({
+         /* ===============
+               COURSES
+         =============== */
+    getCourses: build.query<Course[], { category?: string }>({
+      query: ({ category }) => ({
+        url: "courses",
+        params: { category },
+      }),
+      providesTags: ["Courses"],
+    }),
+
+    getCourse: build.query<Course, string>({
+      query: (id) => `courses/${id}`,
+      providesTags: (result, error, id) => [{ type: "Courses", id }],
+    }),
+    }),
   });
 
   export const {} = api;
