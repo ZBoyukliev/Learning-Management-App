d9b4f019313d2a527c2102d368ced9ac2940be16
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-05-08 12:47:31 +0300

    create a Лоадинг цомпонент

--- client/src/app/(nondashboard)/search/page.tsx
+++ client/src/app/(nondashboard)/search/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 
+import { useGetCoursesQuery } from "@/state/api";
 import { motion } from "framer-motion";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 const Search = () => {
+
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+  const { data: courses, isLoading, isError } = useGetCoursesQuery({});
+  const [selectedCourse, setSelectedCourse] = useState<Course | null>(null);
+  const router = useRouter();
  
 
   return (
