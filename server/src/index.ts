437cc49b8db1d8a035079b4a6e29b87213e29455
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-05-31 12:51:05 +0300

    add clerk middle ware

--- server/src/index.ts
+++ server/src/index.ts
@@ -33,6 +33,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(clerkMiddleware());
 
 /* ROUTES */
 app.get("/", (req, res) => {
