6289f2c490eb36c6790fcf14c30b2ede040deba1
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-06-22 22:36:38 +0300

    instal multer and create course routes

--- server/src/routes/courseRoutes.ts
+++ server/src/routes/courseRoutes.ts
@@ -1,12 +1,25 @@
 import express from "express";
+import multer from "multer";
 import {
+  createCourse,
+  deleteCourse,
   getCourse,
   listCourses,
+  updateCourse,
 } from "../controllers/courseController";
+import { requireAuth } from "@clerk/express";
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/", listCourses);
-router.get("/:courseId", getCourse);
+router.post("/", requireAuth(), createCourse);
 
-export default router;
+router.get("/:courseId", getCourse);
+router.put("/:courseId", requireAuth(), upload.single("image"), updateCourse);
+router.delete("/:courseId", requireAuth(), deleteCourse);
+
+router.post(
+  "/:courseId/sections/:sectionId/chapters/:chapterId/get-upload-url",
+  requireAuth(),
+);
