bea425f5a98d98783056c1b9244e735ae0e27e32
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-06-26 20:03:29 +0300

    create course page in teachers folder

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import * as z from "zod";
+import { api } from "../state/api";
+import { toast } from "sonner";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -285,3 +287,102 @@ export const customDataGridStyles = {
     color: "#6e6e6e",
   },
 };
+
+export const createCourseFormData = (
+  data: CourseFormData,
+  sections: Section[]
+): FormData => {
+  const formData = new FormData();
+  formData.append("title", data.courseTitle);
+  formData.append("description", data.courseDescription);
+  formData.append("category", data.courseCategory);
+  formData.append("price", data.coursePrice.toString());
+  formData.append("status", data.courseStatus ? "Published" : "Draft");
+
+  const sectionsWithVideos = sections.map((section) => ({
+    ...section,
+    chapters: section.chapters.map((chapter) => ({
+      ...chapter,
+      video: chapter.video,
+    })),
+  }));
+
+  formData.append("sections", JSON.stringify(sectionsWithVideos));
+
+  return formData;
+};
+
+export const uploadAllVideos = async (
+  localSections: Section[],
+  courseId: string,
+  getUploadVideoUrl: any
+) => {
+  const updatedSections = localSections.map((section) => ({
+    ...section,
+    chapters: section.chapters.map((chapter) => ({
+      ...chapter,
+    })),
+  }));
+
+  for (let i = 0; i < updatedSections.length; i++) {
+    for (let j = 0; j < updatedSections[i].chapters.length; j++) {
+      const chapter = updatedSections[i].chapters[j];
+      if (chapter.video instanceof File && chapter.video.type === "video/mp4") {
+        try {
+          const updatedChapter = await uploadVideo(
+            chapter,
+            courseId,
+            updatedSections[i].sectionId,
+            getUploadVideoUrl
+          );
+          updatedSections[i].chapters[j] = updatedChapter;
+        } catch (error) {
+          console.error(
+            `Failed to upload video for chapter ${chapter.chapterId}:`,
+            error
+          );
+        }
+      }
+    }
+  }
+
+  return updatedSections;
+};
+
+async function uploadVideo(
+  chapter: Chapter,
+  courseId: string,
+  sectionId: string,
+  getUploadVideoUrl: any
+) {
+  const file = chapter.video as File;
+
+  try {
+    const { uploadUrl, videoUrl } = await getUploadVideoUrl({
+      courseId,
+      sectionId,
+      chapterId: chapter.chapterId,
+      fileName: file.name,
+      fileType: file.type,
+    }).unwrap();
+
+    await fetch(uploadUrl, {
+      method: "PUT",
+      headers: {
+        "Content-Type": file.type,
+      },
+      body: file,
+    });
+    toast.success(
+      `Video uploaded successfully for chapter ${chapter.chapterId}`
+    );
+
+    return { ...chapter, video: videoUrl };
+  } catch (error) {
+    console.error(
+      `Failed to upload video for chapter ${chapter.chapterId}:`,
+      error
+    );
+    throw error;
+  }
+}
