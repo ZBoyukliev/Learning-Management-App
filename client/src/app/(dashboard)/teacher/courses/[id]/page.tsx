53c6670d15a4fdd9a663797be54a461450fb0f63
ZBoyukliev <zarkoboyukliev@gmail.com>
2025-07-02 11:35:57 +0300

    install plder version on react player

--- client/src/app/(dashboard)/teacher/courses/[id]/page.tsx
+++ client/src/app/(dashboard)/teacher/courses/[id]/page.tsx
@@ -8,13 +8,13 @@ import { courseSchema } from "@/lib/schemas";
 import {
   centsToDollars,
   createCourseFormData,
-//   uploadAllVideos,
+  uploadAllVideos,
 } from "@/lib/utils";
 import { openSectionModal, setSections } from "@/state";
 import {
   useGetCourseQuery,
   useUpdateCourseMutation,
-//   useGetUploadVideoUrlMutation,
+  useGetUploadVideoUrlMutation,
 } from "@/state/api";
 import { useAppDispatch, useAppSelector } from "@/state/redux";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -32,7 +32,7 @@ const CourseEditor = () => {
   const id = params.id as string;
   const { data: course, isLoading, refetch } = useGetCourseQuery(id);
   const [updateCourse] = useUpdateCourseMutation();
-//   const [getUploadVideoUrl] = useGetUploadVideoUrlMutation();
+  const [getUploadVideoUrl] = useGetUploadVideoUrlMutation();
 
   const dispatch = useAppDispatch();
   const { sections } = useAppSelector((state) => state.global.courseEditor);
@@ -63,11 +63,11 @@ const CourseEditor = () => {
 
   const onSubmit = async (data: CourseFormData) => {
     try {
-    //   const updatedSections = await uploadAllVideos(
-    //     sections,
-    //     id,
-    //     getUploadVideoUrl
-    //   );
+      const updatedSections = await uploadAllVideos(
+        sections,
+        id,
+        getUploadVideoUrl
+      );
 
       const formData = createCourseFormData(data, sections);
 
